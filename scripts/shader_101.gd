209135a4ff72be0c49fcb6d014d5954698bbb5ac
nhan.trinh <hongnhan50@gmail.com>
2025-03-09 20:28:53 +0700

    Setup simple project in 4.4

--- scripts/shader_101.gd
+++ scripts/shader_101.gd
@@ -1,5 +1,14 @@
 extends Node
 
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
+#func _process(_delta: float) -> void:
+	#var TIME = Time.get_ticks_msec() / 1000.0
+	#print("TIME: " + str(TIME))
+
 func _process(_delta: float) -> void:
-	var TIME = Time.get_ticks_msec() / 1000.0
-	print("TIME: " + str(TIME))
+	if Input.is_action_just_pressed("set_speed"):
+		var new_speed = randf_range(1.0, 10.0)
+		var new_color = Vector4(1.0, 0.0, 0.0, 1.0)
+		sprite_2d.material.set_shader_parameter("my_color", new_color)
+		#print(new_speed)
